48d3c2367c1ed963ec4b872dd0cbe957363c21f8
xingluokang <3107562389@qq.com>
2022-05-06 08:55:36 +0800

    新增UITabView的自定义BackgroundView。

---
+++ CustomView/CustomView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CustomView
+//
+//  Created by 邢罗康 on 2022/5/6.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
